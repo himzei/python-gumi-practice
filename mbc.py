9e1da3e9d49e0e5b9d5a054d38033baaf7230a01
himzei <himzei@gmail.com>
2024-08-20 09:51:17 +0900

    240820

--- mbc.py
+++ mbc.py
@@ -2,10 +2,10 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-query = "광복절"
+query = "대구"
 search_url = f"https://search.imbc.com/news?qt={query}"
 
-download_folder = f"./{query}"
+download_folder = f"./{query}" # ./전기차
 
 response = requests.get(search_url)
 soup = BeautifulSoup(response.text, "html.parser")
@@ -13,16 +13,22 @@ soup = BeautifulSoup(response.text, "html.parser")
 image_tags = soup.find("ul", class_="list-type news-list")
 lis = image_tags.find_all("li")
 
+
+
 downloaded = 0
 
 if not os.path.exists(download_folder):
     os.makedirs(download_folder)
 
 for img_tag in lis: 
+    # 제목가져오기 
+    # title = img_tag.find("span", class_="nw_tit ellipsis-multi").text
+    # print(title)
     img_url = img_tag.find("img").get("src")
+    print(img_url)
     img_data = requests.get(f"https:{img_url}").content
     img_name = os.path.join(download_folder, f"image_{downloaded + 1}.jpg")
-
+    
     downloaded = downloaded + 1
     # downloaded += 1
 
